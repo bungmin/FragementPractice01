b2c56b01a8a90d0e3dbfa0c0f910a2c99c1ae98d
babobungmin <babobungmin@naver.com>
2019-05-11 10:19:59 +0900

    Initial commit

---
+++ app/src/main/java/com/hh/fragementpractice01/MainActivity.java
@@ -0,0 +1,38 @@
+package com.hh.fragementpractice01;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hh.fragementpractice01.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+    ActivityMainBinding act;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        bindViews();
+        setupEvents();
+        setValues();
+
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+        act = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+    }
+}
