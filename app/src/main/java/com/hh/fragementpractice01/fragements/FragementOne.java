7e0e9d2732e5b6a6c4aa80da3b80822e5cbc1a4a
babobungmin <babobungmin@naver.com>
2019-05-11 11:11:01 +0900

    데이터바인딩이용

---
+++ app/src/main/java/com/hh/fragementpractice01/fragements/FragementOne.java
@@ -0,0 +1,51 @@
+package com.hh.fragementpractice01.fragements;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.hh.fragementpractice01.R;
+import com.hh.fragementpractice01.databinding.Fragment01Binding;
+
+public class FragementOne extends Fragment {
+
+    //Fragem
+    Fragment01Binding binding;
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return inflater.inflate(R.layout.fragment_01, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_01, container, false);
+
+        return  binding.getRoot();
+
+
+
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        /*TextView myTextView = getActivity().findViewById(R.id.myTextView);
+
+        myTextView.setText("JAVA");
+
+        */
+
+        binding.myTextView.setText("데이터 바인딩 이용");
+
+
+
+    }
+}
---
+++ app/src/main/java/com/hh/fragementpractice01/fragements/FragementTwo.java
@@ -0,0 +1,20 @@
+package com.hh.fragementpractice01.fragements;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hh.fragementpractice01.R;
+
+public class FragementTwo extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_02, container, false);
+    }
+}
